131f091e551255151ed99abc368184b69b4e0cf7
Aristidescosta <aristidiscosta200@gmail.com>
2026-05-09 12:43:31 +0100

    fix: remove invalid windows path

--- apps/media_files/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class MediaFilesConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.media_files"
-    verbose_name = "Media_Files"
